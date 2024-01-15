db9fedec1e6f3e9853776dd2a5f77cb678dee650
Bradley Caliva <bcaliva21@gmail.com>
2024-01-14 19:19:38 -0600

    feat(1.5): implement lesson 1.5

---
+++ 1.5/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter two numbers separated by a space: ";
+
+	int x{};
+	int y{};
+	cin >> x >> y;
+
+	cout << "You entered " << x << " and " << y << "\n";
+
+	return 0;
+}
