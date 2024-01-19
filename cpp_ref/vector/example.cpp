ddfed269c0a195680a0806928a15d54395a12f7c
Bradley Caliva <bcaliva21@gmail.com>
2024-01-18 22:28:41 -0600

    feat(cpp_ref/vector): implement example

---
+++ cpp_ref/vector/example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = {8, 4, 5, 9};
+
+	v.push_back(6);
+	v.push_back(9);
+
+	auto it = v.begin();
+	it = v.insert(it, 420);
+
+	v[2] = -1;
+
+	for (int n : v)
+		std::cout << n << '\n';
+	std::cout << '\n';
+	std::cout << "expect: [420,8,-1,5,9,6,9] \n";
+}
